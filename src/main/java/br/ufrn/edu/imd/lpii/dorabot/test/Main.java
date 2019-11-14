bb78fbf0023cdc5495652e936ed557e22e8f263b
Jâncy Aragão <jancy.aragao@gmail.com>
2019-11-14 16:48:11 -0300

    Refatoração do método de recebimento dos comandos

--- src/main/java/br/ufrn/edu/imd/lpii/dorabot/test/Main.java
+++ src/main/java/br/ufrn/edu/imd/lpii/dorabot/test/Main.java
@@ -2,6 +2,7 @@ package br.ufrn.edu.imd.lpii.dorabot.test;
 
 import java.util.List;
 
+import com.google.protobuf.Message;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ChatAction;
@@ -12,22 +13,30 @@ import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 
+import br.ufrn.edu.imd.lpii.dorabot.model.Estados;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		//Criação do objeto bot com as informações de acesso
+		//Criaï¿½ï¿½o do objeto bot com as informaï¿½ï¿½es de acesso
 		TelegramBot bot = new TelegramBot("1027972095:AAHb35m5Al2v7hee1lzh1dTS_XYOLQkl7C8");
 
-		//objeto responsável por receber as mensagens
+		//objeto responsï¿½vel por receber as mensagens
 		GetUpdatesResponse updatesResponse;
-		//objeto responsável por gerenciar o envio de respostas
+		//objeto responsï¿½vel por gerenciar o envio de respostas
 		SendResponse sendResponse;
-		//objeto responsável por gerenciar o envio de ações do chat
+		//objeto responsï¿½vel por gerenciar o envio de aï¿½ï¿½es do chat
 		BaseResponse baseResponse;
 		
-		//controle de off-set, isto é, a partir deste ID será lido as mensagens pendentes na fila
+		//controle de off-set, isto ï¿½, a partir deste ID serï¿½ lido as mensagens pendentes na fila
 		int m=0;
+		
+		Estados estado = Estados.NULO;
+		String nome = "";
+		String descricao = "";
+		String cod_localizacao = "";
+		String cod_categoria = "";
 		
 		//loop infinito pode ser alterado por algum timer de intervalo curto
 		while (true){
@@ -38,23 +47,65 @@ public class Main {
 			//lista de mensagens
 			List<Update> updates = updatesResponse.updates();
 
-			//análise de cada ação da mensagem
+			//anï¿½lise de cada aï¿½ï¿½o da mensagem
 			for (Update update : updates) {
 				
-				//atualização do off-set
-				m = update.updateId()+1;
-				
-				System.out.println("Recebendo mensagem:"+ update.message().text());
-				
-				//envio de "Escrevendo" antes de enviar a resposta
-				baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
-				//verificação de ação de chat foi enviada com sucesso
-				System.out.println("Resposta de Chat Action Enviada?" + baseResponse.isOk());
-				
-				//envio da mensagem de resposta
-				sendResponse = bot.execute(new SendMessage(update.message().chat().id(),"Agora entendi..."));
-				//verificação de mensagem enviada com sucesso
-				System.out.println("Mensagem Enviada?" +sendResponse.isOk());
+				if (estado == Estados.NULO) {
+					
+					if(update.message().text().equals("/start")) {
+						baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+						sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "OlÃ¡! Eu sou a Dora. Use os comandos disponÃ­veis para interagir comigo! :)"));
+					} else if(update.message().text().equals("/cadastrarbem")) {
+						baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+						sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "Informe o nome do bem:"));
+						estado = Estados.ESPERA_NOME_BEM;
+					} else if(update.message().text().equals("/cadastrarlocalizacao")) {
+						baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+						sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "Informe o nome da categoria:"));
+						estado = Estados.ESPERA_NOME_LOCALIZACAO;
+					} else if(update.message().text().equals("/cadastrarcategoria")) {
+						baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+						sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "Informe o nome da categoria:"));
+						estado = Estados.ESPERA_NOME_CATEGORIA;
+					} else if(update.message().text().equals("/gerarRelatorio")) {
+						
+					} else if(update.message().text().equals("/listarbens")) {
+						
+					} else if(update.message().text().equals("/listarlocalizacoes")) {
+						
+					} else if(update.message().text().equals("/listarcategorias")) {
+						
+					} else if(update.message().text().equals("/listarbensporlocalizacao")) {
+						
+					} else if(update.message().text().equals("/buscarbemcodigo")) {
+						
+					} else if(update.message().text().equals("/buscarbemnome")) {
+						
+					} else if(update.message().text().equals("/buscarbemdescricao")) {
+					
+					} else if(update.message().text().equals("/movimentarbem")) {
+						
+					} else if(update.message().text().equals("/obrigada")) {
+						
+					}
+					
+				} else if (estado == estado.ESPERA_NOME_BEM) {
+					
+				} else if (estado == estado.ESPERA_DESCRICAO_BEM) {
+					
+				} else if (estado == estado.ESPERA_LOCALIZACAO_BEM) {
+					
+				} else if (estado == estado.ESPERA_CATEGORIA_BEM) {
+					
+				} else if (estado == estado.ESPERA_NOME_LOCALIZACAO) {
+					
+				} else if (estado == estado.ESPERA_DESCRICAO_LOCALIZACAO) {
+					
+				} else if (estado == estado.ESPERA_NOME_CATEGORIA) {
+					
+				} else if (estado == estado.ESPERA_DESCRICAO_CATEGORIA) {
+					
+				}
 				
 			}
 
