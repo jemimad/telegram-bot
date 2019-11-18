4ab9c83aec58556eb986fbfdca4f35cf7b689c0c
Jâncy Aragão <jancy.aragao@gmail.com>
2019-11-18 11:43:33 -0300

    Resolução de conflitos

--- src/main/java/br/ufrn/edu/imd/lpii/dorabot/dao/AbstractDAO.java
+++ src/main/java/br/ufrn/edu/imd/lpii/dorabot/dao/AbstractDAO.java
@@ -3,11 +3,16 @@ package br.ufrn.edu.imd.lpii.dorabot.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+/**
+ * Classe respons�vel pela conex�o com o banco de dados.
+ */
 public abstract class AbstractDAO {
 
 	protected Connection conexao;
 	
+	/**
+	 * Inicia a conex�o com o banco.
+	 */
 	public AbstractDAO() {
 		try {
 			conexao = DriverManager.getConnection("jdbc:mysql://localhost/ARQUIVO?useSSL=true&useTimezone=true&serverTimezone=UTC", "USER", "PASSWORD");
@@ -15,7 +20,10 @@ public abstract class AbstractDAO {
 			System.out.println("Não foi possível conectar ao banco de dados: \n\t" + e);
 		}
 	}
-
+	
+	/** 
+	 * Finaliza a conex�o com o banco.
+	 */
 	public void fechar() {
 		try {
 			conexao.close();
